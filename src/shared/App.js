15cdff212e31f278656c7b56b9203275ad3578c3
rectanglehihat <80513184+rectanglehihat@users.noreply.github.com>
2021-07-11 21:31:26 +0900

    없는 페이지 처리 NotFound컴포넌트 생성 및 연결

--- src/shared/App.js
+++ src/shared/App.js
@@ -1,10 +1,12 @@
 import React from "react";
-import {BrowserRouter, Route} from "react-router-dom";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
 import styled from "styled-components";
 
+import NotFound from "./NotFound";
 import Signup from "../pages/Signup";
 import PostWrite from "../pages/PostWrite"
 import PostDetail from "../pages/PostDetail";
+import PostList from "../pages/PostList";
 
 import GlobalStyles from "./GlobalStyles";
 
@@ -14,9 +16,20 @@ function App() {
     <React.Fragment>
         <GlobalStyles />
         <BrowserRouter>
-          <Route path="/signup" exact component={Signup} />
-          <Route path="/write" exact component={PostWrite} />
-          <Route path="/post" exact component={PostDetail} />
+          <Switch>
+            <Route path="/signup" exact component={Signup} />
+            <Route path="/write" exact component={PostWrite} />
+            {/* 게시글 하나 상세 페이지 */}
+            <Route path="/post/:id" exact component={PostDetail} />
+            {/* 게시글 모두 나오는 전체 페이지 */}
+            <Route path="/post" exact component={PostList} />
+            {/* 없는 페이지 처리하려면 Switch로 감싼 다음에 라우트로 path를 넘겨주지 않으면
+            path가 지정되지 않은 모든 주소는 NotFound로 빠짐 */}
+            <Route render={(props) => (
+                <NotFound history={props.history}/>
+              )}/>
+
+          </Switch>
         </BrowserRouter>
     </React.Fragment>
   );
---
+++ src/shared/NotFound.js
@@ -0,0 +1,32 @@
+import React from "react";
+import styled from "styled-components";
+
+import Button from "../elements/Button";
+import Grid from "../elements/Grid";
+import Text from "../elements/Text";
+
+
+const NotFound = (props) => {
+    console.log(props);
+
+  return (
+        <Wrapper>
+            <Grid padding="18px">
+                <Text size="36px" bold>주소가 올바르지 않아요!</Text>
+            </Grid>
+
+            <Button 
+            width="28em"
+            _onClick={() => {props.history.goBack();}}
+            >뒤로가기</Button>
+        </Wrapper>
+  )
+  
+};
+
+const Wrapper = styled.div`
+    padding: 10em;
+    text-align: center;
+`;
+
+export default NotFound;
